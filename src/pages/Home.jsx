096d43c606cb5642410ee1e3059bc93662ac110a
mahanth111 <mahanthkantipudi@gmail.com>
2025-11-20 17:01:04 +0530

    changes in dashboard display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,17 @@ import { Charts } from '../components/Charts';
 
 const Home = ({ transactions }) => {
   return (
-    <>
-      <div className="balance-card">
+    <div style = {{display:"flex",justifyContent:"space-evenly", alignItems: "center", height: "100%", gap:"10px"}}>
+      <div className="balance-card"
+      style={{ flex:1 }}>
         <Balance transactions={transactions} />
-      </div>
+
       <IncomeExpenses transactions={transactions} />
+      </div>
+      <div style = {{flex: 2}}>
       <Charts transactions={transactions} />
-    </>
+      </div>
+    </div>
   );
 };
 
